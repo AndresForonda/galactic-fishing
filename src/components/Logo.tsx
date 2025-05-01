5da1cf06fa3f0110e152a9eaeb54c566bf56b1a0
AndresForonda <andresforonda.af@gmail.com>
2025-05-01 17:02:38 -0500

    improve: update Logo component to use text instead of images

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,8 @@
 const Logo = () => {
   return (
-    <div class="h-auto bg-terminal z-50  text-primary">
+    <div class="h-auto bg-blue-950 z-50  text-primary">
       <div class="xxs:hidden font-terminal text-5xl">BLOQUE FISHING</div>
-      <pre class="hidden  xxs:block xxs:text-[clamp(0.35rem,1.2vw,1.5rem)] xs:text-[clamp(0.4rem,1.2vw,1.5rem)] sm:text-[clamp(0.5rem,1.2vw,.7rem)] lg:text-[clamp(0.32rem,1.2vw,.7rem)] leading-[1.1] sm:leading-[1.2]  whitespace-pre-wrap text-center tracking-tight">
+      <pre class="hidden xxs:block xxs:text-[clamp(0.35rem,1.2vw,1.5rem)] xs:text-[clamp(0.4rem,1.2vw,1.5rem)] sm:text-[clamp(0.5rem,1.2vw,.7rem)] lg:text-[clamp(0.32rem,1.2vw,.7rem)] leading-[1.1] sm:leading-[1.2]  whitespace-pre-wrap text-center tracking-tight">
         {`
 ██████╗ ██╗      ██████╗  ██████╗ ██╗   ██╗███████╗    ███████╗██╗███████╗██╗  ██╗██╗███╗   ██╗ ██████╗ 
 ██╔══██╗██║     ██╔═══██╗██╔═══██╗██║   ██║██╔════╝    ██╔════╝██║██╔════╝██║  ██║██║████╗  ██║██╔════╝ 
